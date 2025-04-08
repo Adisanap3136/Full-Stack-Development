548fde7a97cb6ede80c1f33a660b1cd78ccce7ca
Adisanap3136 <154910450+Adisanap3136@users.noreply.github.com>
2025-04-09 01:21:06 +0530

    Encapsulation

---
+++ JAVA/TheoryCodes/26) OOP/08-04-25 Encapsulation/PatientDetails.java
@@ -0,0 +1,47 @@
+package principleOfOop;
+
+public class PatientDetails {
+	String patientName;
+	int pid;
+	long phone;
+	String padd;
+	String phistory;
+	public PatientDetails(String patientName, int pid, long phone, String padd) {
+		this.patientName = patientName;
+		this.pid = pid;
+		this.phone = phone;
+		this.padd = padd;
+	}
+	public String getPatientName() {
+		return patientName;
+	}
+	public int getPid() {
+		return pid;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public String getPadd() {
+		return padd;
+	}
+	public String getPhistory() {
+		return phistory;
+	}
+	public String getHistory()
+	{
+		return phistory;
+	}
+	public void setHistory(String history)
+	{
+		if(!(pid==0))
+		{
+			phistory = history;
+			System.out.println("Patient history added successfully!");
+		}
+		else
+		{
+			System.out.println("Patient Not Found");
+		}
+	}
+		
+}
